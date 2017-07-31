6bd7c05cbee1d3ca7f7fee043517cfdc1e9e82cc
arjunshelar <arjun.shelar@gmail.com>
2017-07-31 16:31:23 +0530

    checking conflicts

--- src/TestGIt.java
+++ src/TestGIt.java
@@ -4,7 +4,8 @@ public class TestGIt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("Hello Git");
+		System.out.println("Hello asdf");
+	System.out.println("Hello asdf");
 		
 	}
 
